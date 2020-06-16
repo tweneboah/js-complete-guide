8c6a563091eff34925ad182e7d8fbd5ae05242fb
Emmanuel <twentekghana@gmail.com>
2020-06-16 09:00:50 +0800

    Variables

---
+++ variables/app.js
@@ -0,0 +1,22 @@
+//variables
+
+//creating variables
+// var firstName = "Ben";
+// var $firstname = "Ben 2";
+// var first_name = "Ben 4";
+
+var age, message, account;
+age = 30;
+message = "You are welcome";
+
+//retrieving variables
+
+//Methods use to retrieve data
+//console.log(); //display into the browswer
+//alert(); //pop up only
+//prompt(); //pop but it allows you to enter data
+//document.write(); //write to html page
+
+//console.log(firstName);
+//alert(firstName);
+//document.write(firstName);
